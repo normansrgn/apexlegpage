e481e2bf87c8622294c9da52398f3f2b50c6f572
normansrgn <normanmagic2004@gmail.com>
2024-04-16 01:23:27 +0300

    $

---
+++ js/slider.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+    slidesPerView: 1,
+
+    // Autoplay
+    autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
